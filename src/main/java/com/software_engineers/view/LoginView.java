33f1e5f1c9ed97727d0e59a2a678c23f890ac8a6
Rolando <rolando.juarez.327@my.csun.edu>
2026-08-12 07:48:22 -0700

    Added comments to reference code for refactoring

--- src/main/java/com/software_engineers/view/LoginView.java
+++ src/main/java/com/software_engineers/view/LoginView.java
@@ -1,9 +1,14 @@
 package com.software_engineers.view;
 
+import com.software_engineers.model.User;
 import com.software_engineers.service.LoginRegService;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 public class LoginView extends VBox {
@@ -19,4 +24,48 @@ public class LoginView extends VBox {
         passwordField.setPromptText("Password");
 
     }
+
+    // @formatter:off
+    // Original author: Faith
+    // void rand() {
+    //     GridPane loginGP = new GridPane();
+
+    //     Label usernameLbl = new Label("Username: ");
+    //     TextField usernameField = new TextField();
+
+    //     Label passwordLbl = new Label("Password: ");
+    //     PasswordField passwordField = new PasswordField();
+
+    //     Button loginButton = new Button("Login");
+
+    //     loginGP.add(usernameLbl, 0, 0);
+    //     loginGP.add(usernameField, 1, 0);
+
+    //     loginGP.add(passwordLbl, 0, 1);
+    //     loginGP.add(passwordField, 1, 1);
+
+    //     loginGP.add(loginButton, 0, 2);
+
+    //     loginButton.setOnAction(e -> {
+    //         String username = usernameField.getText();
+    //         String password = passwordField.getText();
+
+    //         userDao.verifyLogin(username, password);
+
+    //         if (true) {
+    //             User theUser = userDao.getUserByUsername(username);
+    //             curUserId = theUser.getId();
+    //             cartDAO.clearCart(curUserId);
+    //             itemsScene = new Scene(gp, 640, 480);
+    //             stage.setScene(itemsScene);
+
+    //         }
+
+    //     });
+
+    //     Scene loginScene = new Scene(loginGP, 640, 480);
+    //     stage.setScene(loginScene);
+    //     stage.show();
+    // }
+    // @formatter:on
 }
